7593df376e830568f8bc696eb0048c2172cd904b
Kenneth Jordan <kensworldc@gmail.com>
2023-03-02 07:53:30 -0500

    optimizing code

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,7 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div 
-  id='about'><h2>About Me</h2>
-  <p>Lorem ipsum dolor sit amet</p>
-  <img src= {image} alt='I made this'></img>
+  return <div id='about'>
   About
   </div>;
  
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>
-    <a href="#home">Home</a>
-    <a href="#about">About</a>
-    </nav>;
+  return
+  (
+   
+  );
 }
 
 export default NavBar;
